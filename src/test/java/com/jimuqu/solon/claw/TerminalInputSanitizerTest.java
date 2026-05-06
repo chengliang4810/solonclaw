73d65a72efc21dac859b92592ab925b845d0b025
chengliang <chengliang4810@163.com>
2026-05-07 00:57:40 +0800

    test: cover terminal input control leaks

--- src/test/java/com/jimuqu/solon/claw/TerminalInputSanitizerTest.java
+++ src/test/java/com/jimuqu/solon/claw/TerminalInputSanitizerTest.java
@@ -10,18 +10,30 @@ public class TerminalInputSanitizerTest {
     void shouldPreservePlainText() {
         assertThat(TerminalInputSanitizer.stripLeakedTerminalResponses("hello world"))
                 .isEqualTo("hello world");
+        assertThat(TerminalInputSanitizer.stripLeakedTerminalResponses(""))
+                .isEqualTo("");
         assertThat(TerminalInputSanitizer.stripLeakedTerminalResponses("see [53;1R section"))
                 .isEqualTo("see [53;1R section");
         assertThat(TerminalInputSanitizer.stripLeakedTerminalResponses("\u001B[31mred\u001B[0m"))
                 .isEqualTo("\u001B[31mred\u001B[0m");
+        assertThat(
+                        TerminalInputSanitizer.stripLeakedTerminalResponses(
+                                "render <div class='hero'> literal"))
+                .isEqualTo("render <div class='hero'> literal");
     }
 
     @Test
     void shouldStripDsrResponsesLikeHermesCli() {
         assertThat(TerminalInputSanitizer.stripLeakedTerminalResponses("\u001B[53;1R"))
                 .isEqualTo("");
+        assertThat(TerminalInputSanitizer.stripLeakedTerminalResponses("^[[53;1R"))
+                .isEqualTo("");
         assertThat(TerminalInputSanitizer.stripLeakedTerminalResponses("hello\u001B[53;1Rworld"))
                 .isEqualTo("helloworld");
+        assertThat(
+                        TerminalInputSanitizer.stripLeakedTerminalResponses(
+                                "a\u001B[53;1Rb\u001B[51;1Rc\u001B[50;9Rd"))
+                .isEqualTo("abcd");
         assertThat(TerminalInputSanitizer.stripLeakedTerminalResponses("typed^[[53;1Rmore"))
                 .isEqualTo("typedmore");
         assertThat(
@@ -38,9 +50,15 @@ public class TerminalInputSanitizerTest {
                 .isEqualTo("abcdef");
         assertThat(TerminalInputSanitizer.stripLeakedTerminalResponses("abc<65;1;49Mdef"))
                 .isEqualTo("abcdef");
+        assertThat(
+                        TerminalInputSanitizer.stripLeakedTerminalResponses(
+                                "abc\u001B[<10000;12345;98765Mdef"))
+                .isEqualTo("abcdef");
         assertThat(TerminalInputSanitizer.stripLeakedTerminalResponses("<65;1;49M<35;1;42Mhello"))
                 .isEqualTo("hello");
-        assertThat(TerminalInputSanitizer.stripLeakedTerminalResponses("render <div> literal"))
-                .isEqualTo("render <div> literal");
+        assertThat(
+                        TerminalInputSanitizer.stripLeakedTerminalResponses(
+                                "<65;1;49M<35;1;42Mhello<64;1;40m"))
+                .isEqualTo("hello");
     }
 }
